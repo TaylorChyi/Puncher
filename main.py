f837d929a0bf9625533bc110e5b8f3248811e5cc
Orange-66 <qity_66@outlook.com>
2021-02-19 17:57:26 +0800

    - 测试-2；

--- main.py
+++ main.py
@@ -15,20 +15,17 @@ password = browser.find_element_by_id('pd')
 username.send_keys(settings.username)
 password.send_keys(settings.password)
 
-try:
-    element = WebDriverWait(browser, 10).until(
-        EC.presence_of_element_located((By.XPATH, '//*[@id="index_login_btn"]'))
-    )
-    index_login_btn = browser.find_element_by_id('index_login_btn')
-    index_login_btn.click()
+index_login_btn = browser.find_element_by_id('index_login_btn')
+index_login_btn.click()
 
-    by_yourself_spin = browser.find_elements_by_name('jibenxinxi_shifoubenrenshangbao')[0]
+try:
+    by_yourself_spin = browser.find_elements_by_class_name('el-radio__spin')[0]
     by_yourself_spin.click()
 
-    health_state_spin = browser.find_elements_by_name('jiankangxinxi_muqianshentizhuangkuang')[0]
+    health_state_spin = browser.find_elements_by_class_name('el-radio__spin')[4]
     health_state_spin.click()
 
-    journey_info_spin = browser.find_elements_by_id('xingchengxinxi_weizhishifouyoubianhua')[0]
+    journey_info_spin = browser.find_elements_by_class_name('el-radio__spin')[12]
     journey_info_spin.click()
 
     submit_btn = browser.find_element_by_class_name('el-button Findbutton el-button--primary')
