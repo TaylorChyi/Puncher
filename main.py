663cf81bb3761325d3869e7ff8bc6dbfbdd6cc35
Orange-66 <qity_66@outlook.com>
2021-02-19 18:03:47 +0800

    - 测试-4；

--- main.py
+++ main.py
@@ -19,16 +19,16 @@ index_login_btn = browser.find_element_by_id('index_login_btn')
 index_login_btn.click()
 
 try:
-    by_yourself_spin = browser.find_elements_by_class_name('el-radio__spin')[0]
+    by_yourself_spin = browser.find_elements_by_class_name('el-radio__inner')[0]
     by_yourself_spin.click()
 
-    health_state_spin = browser.find_elements_by_class_name('el-radio__spin')[4]
+    health_state_spin = browser.find_elements_by_class_name('el-radio__inner')[4]
     health_state_spin.click()
 
-    journey_info_spin = browser.find_elements_by_tag_name('button')[1]
+    journey_info_spin = browser.find_elements_by_class_name('el-radio__inner')[12]
     journey_info_spin.click()
 
-    submit_btn = browser.find_element_by_class_name('el-button Findbutton el-button--primary')
+    submit_btn = browser.find_elements_by_tag_name('button')[1]
     submit_btn.click()
 finally:
     browser.close()
